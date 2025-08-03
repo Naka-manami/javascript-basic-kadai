103698b87763dc0c897d67ed66c796b7fc068a79
Naka-manami <151598224+Naka-manami@users.noreply.github.com>
2025-08-03 17:07:49 +0900

    第５章の課題を提出します

---
+++ kadai_005/bmi.js
@@ -0,0 +1,15 @@
+function bmiValue(){
+  let weight = getInputValue("weight");
+  let height = getInputValue("height");
+  let bmi = calculateSum(weight , height);
+  document.getElementById("result").textContent = bmi;
+}
+// 指定されたIDのinput要素の値を数値として取得する関数
+function getInputValue(bmi) {
+  return Number(document.getElementById(bmi).value);
+}
+// 合計を計算する関数
+function calculateSum(weight, height) {
+  console.log(weight / (height * height))
+  return weight / (height * height);
+}
